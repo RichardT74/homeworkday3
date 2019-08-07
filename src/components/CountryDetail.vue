3bfb288786b58d510f77dec3e8151f670425ddbc
RichardT74 <richtrist@gmail.com>
2019-08-07 17:00:32 +0100

    initial commit

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+<div v-if='country'>
+	<h3>{{country.name}}</h3>
+	<p>Sub Region: {{country.subregion}}</p>
+  <p>Region: {{country.region}}</p>
+  <p>Population: {{country.population}}</p>
+
+
+</div>
+</template>
+
+<script>
+export default {
+	name:'country-detail',
+	props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <li v-on:click="handleClikck">{{country.name}}</li>
+</template>
+
+<script>
+import { eventBus } from '../main.js'
+
+export default {
+  name: 'list-item',
+  props: ['country'],
+  methods: {
+    handleClikck(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
